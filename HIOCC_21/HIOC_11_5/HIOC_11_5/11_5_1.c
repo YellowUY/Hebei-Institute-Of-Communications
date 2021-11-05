a72a80560a01a183a22d78a46803740c4797bd53
Yellow_UY <26238112@qq.com>
2021-11-05 17:59:50 +0800

    计算和差积商

---
+++ HIOCC_21/HIOC_11_5/HIOC_11_5/11_5_1.c
@@ -0,0 +1,24 @@
+//河北传媒学院_程序设计基础_P32实战训练输出两数之和差积商_11_5_Deng
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int main()
+//{
+//	double a, b, sum, del, pro, quo;
+//	printf("请输第一个数：");
+//	scanf("%lf", &a);
+//	printf("请输第二个数：");
+//	scanf("%lf", &b);
+//	sum = a + b;
+//	del = a - b;
+//	pro = a * b;
+//	quo = a / b;
+//	if (b == 0)
+//	{
+//		printf("两数之和=%f 两数之差=%f 两数之积=%f 两数之商不存在", sum, del, pro);
+//	}
+//	else
+//	{
+//		printf("两数之和=%f 两数之差=%f 两数之积=%f 两数之商=%f", sum, del, pro, quo);
+//	}
+//	return 0;
+//}
